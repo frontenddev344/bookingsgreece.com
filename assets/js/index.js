a5aefa000f5ded38b91a13dc96cd646a25c11d0e
Dev avology <devavology12@gmail.com>
2024-08-22 16:39:17 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -39,5 +39,33 @@ AOS.init();
 
 // AOS JS End
 
+//Cookies JS Start
+const cookiesBox = document.querySelector('.wrapper'),
+  buttons = document.querySelectorAll('.button');
+
+// ---- ---- Show ---- ---- //
+const executeCodes = () => {
+  if (document.cookie.includes('AlexGolovanov')) return;
+  cookiesBox.classList.add('show');
+
+  // ---- ---- Button ---- ---- //
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      cookiesBox.classList.remove('show');
+
+      // ---- ---- Time ---- ---- //
+      if (button.id == 'acceptBtn') {
+        document.cookie =
+          'cookieBy= AlexGolovanov; max-age=' + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+
+window.addEventListener('load', executeCodes);
+
+// //Cookies JS Start
+
+
 
 
